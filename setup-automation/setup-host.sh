6f525b11f896a61f9117be94d40753e49132e813
Matthew Yee <myee@redhat.com>
2025-07-31 15:06:00 -0700

    Remove commented-out commands in setup-host.sh and streamline repository settings

--- setup-automation/setup-host.sh
+++ setup-automation/setup-host.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-exit 0
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-    echo "Waiting for Instruqt to finish booting the VM"
-    sleep 1
-done
 
-# virsh start rhel08
-
-# echo "${SSHKEY}" > /root/.ssh/id_rsa
-# chmod 600 /root/.ssh/id_rsa
-# ssh-keygen -f /root/.ssh/id_rsa -y > /root/.ssh/id_rsa.pub
-
-# subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
-
-# # Attempt to register the vm 3 times, waiting 30 seconds for each attempt.
-# (r=3;while ! ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" root@rhel08 "subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force" ; do ((--r))||exit;sleep 30;done)
-
-# ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" root@rhel08 "sed -i 's/^# %wheel.*/%wheel        ALL=(ALL)       NOPASSWD: ALL/' /etc/sudoers"
 subscription-manager repos --disable=*
 subscription-manager repos --enable rhel-8-for-x86_64-baseos-rpms
 subscription-manager repos --enable rhel-8-for-x86_64-appstream-rpms
